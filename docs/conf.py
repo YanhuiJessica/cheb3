66d765a5403d507992dcb3f07c0fd34d382239e0
YanhuiJessica <137126578@qq.com>
2023-04-01 17:17:50 +0800

    init docs

---
+++ docs/conf.py
@@ -0,0 +1,41 @@
+# -- Project information -----------------------------------------------------
+
+project = "cheb3"
+copyright = "2023, YanhuiJessica"
+author = "YanhuiJessica"
+
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+]
+
+intersphinx_mapping = {
+    "web3py": ("https://web3py.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+}
+
+templates_path = ["_templates"]
+
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,7 @@
+.. meta::
+    :description: Web3 CTF tool based on web3.py
+
+Introduction
+============
+
+🐣
